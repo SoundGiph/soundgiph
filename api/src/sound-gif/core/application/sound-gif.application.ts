dd6bb43f5fe374265eebfef2820b1a2399c7b8c5
eMassou <erwan@libeo.io>
2021-12-04 16:56:53 +0100

    feature(module): added new query in module

--- api/src/sound-gif/core/application/sound-gif.application.ts
+++ api/src/sound-gif/core/application/sound-gif.application.ts
@@ -1,5 +1,6 @@
+import { FindMostRecentSoundGifQuery } from './queries/find-most-recent-sound-gif/find-most-recent-sound-gif.query';
 import { FindSoundGifQuery } from './queries/find-sound-gif/find-sound-gif.query';
 
-const SoundGifQueryHandler = [FindSoundGifQuery];
+const SoundGifQueryHandler = [FindSoundGifQuery, FindMostRecentSoundGifQuery];
 
 export const SoundGifApplications = [...SoundGifQueryHandler];
