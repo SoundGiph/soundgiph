0e85518e96455b8679d1ef6420a6526915167bbb
eMassou <erwan@libeo.io>
2021-12-19 03:13:09 +0100

    wip

--- web/components/SoundGifsList/SoundGifsList.tsx
+++ web/components/SoundGifsList/SoundGifsList.tsx
@@ -1,4 +1,3 @@
-import { useTranslation } from "react-i18next";
 import { SoundgifDTO } from "../../domain/sound-gif.dto";
 import { SoundGifsListRow } from "./SoundGifsListRow/SoundGifsListRow";
 
@@ -10,7 +9,7 @@ export const SoundGifsList: React.FC<SoundGifsListProps> = ({ soundGifs }) => {
   if (!soundGifs) return null;
   return (
     <div className="w-11/12 h-full px-4 py-10 bg-cover card bg-base">
-      <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 xl:grid-cols-4 grid justify-items-center h-full w-full">
+      <div className="grid grid-flow-row grid-cols-1 sm:grid-cols-2  xl:grid-cols-4 grid justify-items-center h-full w-full">
         {soundGifs.map((soundGif) => {
           return <SoundGifsListRow soundGif={soundGif} />;
         })}
--- web/components/SoundGifsList/SoundGifsListRow/SoundGifsListRow.tsx
+++ web/components/SoundGifsList/SoundGifsListRow/SoundGifsListRow.tsx
@@ -11,15 +11,17 @@ export const SoundGifsListRow: React.FC<SoundGifsListRowProps> = ({
   const { imageUrl, description, title } = soundGif;
   const { t } = useTranslation();
   return (
-    <div className="flex items-center h-5/6 w-5/6 px-5 py-5 bg-cover card bg-primary m-5">
-      <div className="card glass card-body text-neutral-content h-full w-full items-center justify-between">
+    <div className="flex items-center h-5/6 w-5/6 px-5 py-5 bg-cover card bg-primary m-5 justify-between">
+      <div className="card glass card-body text-neutral-content h-full w-full items-center justify-between compact">
         <div className="avatar">
-          <div className="rounded-full w-24 h-24 ring ring-primary ring-offset-base-100 ring-offset-2">
+          <div className="rounded-full w-28 h-28 ring ring-primary ring-offset-base-100 ring-offset-2">
             <img src={imageUrl} />
           </div>
         </div>
-        <div className="card-actions w-full flex justify-center text-center">
+        <div className="text-center mt-8">
           <p>{description}</p>
+        </div>
+        <div className="card-actions w-full flex justify-center">
           <button className="btn glass rounded-full w-full">
             {t("share")}
           </button>
