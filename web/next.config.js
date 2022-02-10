60373d6261b624e11486255f9464baca76648e1d
Stephane Manganneau <manganneaustephane@gmail.com>
2022-02-10 23:30:45 +0100

    update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -3,8 +3,5 @@ const { i18n } = require("./next-i18next.config");
 
 module.exports = {
   reactStrictMode: true,
-  i18n,
-  env: {
-    API_URL: process.env.ENV === "development" ? process.env.API_URL_DEV : process.env.API_URL_PROD,
-  },
+  i18n
 };
