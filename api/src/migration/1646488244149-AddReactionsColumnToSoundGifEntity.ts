57cc6bf405b5630283e1f43040a57f9f4108f922
eMassou <erwan@libeo.io>
2022-03-05 14:55:05 +0100

    fix(): remove duplicated lines

--- api/src/migration/1646488244149-AddReactionsColumnToSoundGifEntity.ts
+++ api/src/migration/1646488244149-AddReactionsColumnToSoundGifEntity.ts
@@ -7,15 +7,11 @@ export class AddReactionsColumnToSoundGifEntity1646488244149
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "sound_gif" ADD "categories" text NOT NULL`,
-    );
-    await queryRunner.query(
       `ALTER TABLE "sound_gif" ADD "reactions" text NOT NULL`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "sound_gif" DROP COLUMN "reactions"`);
-    await queryRunner.query(`ALTER TABLE "sound_gif" DROP COLUMN "categories"`);
   }
 }
