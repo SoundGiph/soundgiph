d93d7751000ca7b5001b56cac1da48ed8ba3fe54
eMassou <erwan@libeo.io>
2022-03-05 18:45:31 +0100

    feature(infra): added queryBuilder to search a vozo

---
+++ api/src/sound-gif/infrastructure/utils/searchSoundGifQueryBuilder.ts
@@ -0,0 +1,34 @@
+import { Repository } from 'typeorm';
+import { SearchFilter } from '../../core/application/queries/find-sound-gif/find-sound-gif.query';
+import { SoundGifEntity } from '../../core/domain/sound-gif.entity';
+
+export const searchSoundGifQuery = async (
+  soundGifRepository: Repository<SoundGifEntity>,
+  filters?: SearchFilter,
+  fulltext?: string,
+): Promise<SoundGifEntity[]> => {
+  const soundGifsQuery = soundGifRepository.createQueryBuilder('vozo');
+
+  if (filters?.category) {
+    soundGifsQuery.where('vozo.categories ILIKE :category', {
+      category: `%${filters.category}%`,
+    });
+  }
+
+  if (filters?.reaction) {
+    soundGifsQuery.where('vozo.reactions ILIKE :reaction', {
+      reaction: `%${filters.reaction}%`,
+    });
+  }
+
+  if (fulltext) {
+    soundGifsQuery.andWhere(
+      'vozo.description ILIKE :text OR vozo.title ILIKE :text OR vozo.tags ILIKE :text',
+      {
+        text: `%${fulltext}%`,
+      },
+    );
+  }
+
+  return soundGifsQuery.getMany();
+};
