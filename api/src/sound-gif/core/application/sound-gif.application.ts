5a7fc524f0a6cd3c2fec69666f29449809ea3ab7
eMassou <erwan@libeo.io>
2022-03-12 15:29:29 +0100

    feature(query): added new query to get all categories

--- api/src/sound-gif/core/application/sound-gif.application.ts
+++ api/src/sound-gif/core/application/sound-gif.application.ts
@@ -1,19 +1,18 @@
-import { CreateSoundGifCommandHandler } from './commands/create-sound-gif/create-sound-gif.command-handler';
-import { FindMostRecentSoundGifQueryHandler } from './queries/find-most-recent-sound-gif/find-most-recent-sound-gif.query-handler';
-import { FindMostSharedSoundGifQueryHandler } from './queries/find-most-shared-sound-gif/find-most-shared-sound-gif.query-handler';
-import { GetOneSoundGifQueryHandler } from './queries/find-one-sound-gif/find-one-sound-gif.query-handler';
-import { FindSoundGifQueryHandler } from './queries/find-sound-gif/find-sound-gif.query-handler';
+import { CreateSoundGifCommandHandler } from "./commands/create-sound-gif/create-sound-gif.command-handler";
+import { FindMostRecentSoundGifQueryHandler } from "./queries/find-most-recent-sound-gif/find-most-recent-sound-gif.query-handler";
+import { FindMostSharedSoundGifQueryHandler } from "./queries/find-most-shared-sound-gif/find-most-shared-sound-gif.query-handler";
+import { GetOneSoundGifQueryHandler } from "./queries/find-one-sound-gif/find-one-sound-gif.query-handler";
+import { FindSoundGifQueryHandler } from "./queries/find-sound-gif/find-sound-gif.query-handler";
+import { GetAllCategoriesQueryHandler } from "./queries/get-all-categories/get-all-categories.query-handler";
 
 const SoundGifQueryHandler = [
   GetOneSoundGifQueryHandler,
   FindSoundGifQueryHandler,
   FindMostRecentSoundGifQueryHandler,
   FindMostSharedSoundGifQueryHandler,
+  GetAllCategoriesQueryHandler,
 ];
 
 const SoundGifCommandHandler = [CreateSoundGifCommandHandler];
 
-export const SoundGifApplications = [
-  ...SoundGifQueryHandler,
-  ...SoundGifCommandHandler,
-];
+export const SoundGifApplications = [...SoundGifQueryHandler, ...SoundGifCommandHandler];
