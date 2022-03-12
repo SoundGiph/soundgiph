b6d96c7d26d81f04c8435ff3b41ea3cc9f941214
eMassou <erwan@libeo.io>
2022-03-12 15:28:35 +0100

    feature(port/adapter): implement new method to get all categories

--- api/src/sound-gif/infrastructure/sound-gif.adapter.ts
+++ api/src/sound-gif/infrastructure/sound-gif.adapter.ts
@@ -1,58 +1,59 @@
-import { Logger } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { FindOneOptions, Repository } from 'typeorm';
-import { SoundGifPort } from '../core/application/ports/sound-gif.ports';
-import { FindSoundGifPayload } from '../core/application/queries/find-sound-gif/find-sound-gif.query';
-import {
-  SoundGifEntity,
-  SoundGifEntityMandatoryFields,
-} from '../core/domain/sound-gif.entity';
-import { searchSoundGifQuery } from './utils/searchSoundGifQueryBuilder';
+import { Logger } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { FindOneOptions, Repository } from "typeorm";
+import { SoundGifPort } from "../core/application/ports/sound-gif.ports";
+import { FindSoundGifPayload } from "../core/application/queries/find-sound-gif/find-sound-gif.query";
+import { SoundGifEntity, SoundGifEntityMandatoryFields } from "../core/domain/sound-gif.entity";
+import { searchSoundGifQuery } from "./utils/searchSoundGifQueryBuilder";
 
 export class SoundGifAdapter implements SoundGifPort {
   private readonly logger = new Logger();
   constructor(
     @InjectRepository(SoundGifEntity)
-    private readonly soundGifRepository: Repository<SoundGifEntity>,
+    private readonly soundGifRepository: Repository<SoundGifEntity>
   ) {}
 
   public async find(payload: FindSoundGifPayload): Promise<SoundGifEntity[]> {
     const { fulltext, filters } = payload;
     this.logger.log(
-      `SoundGifAdapter > find > called with fulltext: ${fulltext} and filters: ${filters}`,
+      `SoundGifAdapter > find > called with fulltext: ${fulltext} and filters: ${filters}`
     );
-    return await searchSoundGifQuery(
-      this.soundGifRepository,
-      filters,
-      fulltext,
-    );
+    return await searchSoundGifQuery(this.soundGifRepository, filters, fulltext);
   }
 
   public async findMostRecent(): Promise<SoundGifEntity[]> {
-    this.logger.log('SoundGifAdapter > findMostRecent > start');
+    this.logger.log("SoundGifAdapter > findMostRecent > start");
     return await this.soundGifRepository.find({
       order: {
-        createdAt: 'DESC',
+        createdAt: "DESC",
       },
     });
   }
 
   public async getOne(whereOptions: FindOneOptions): Promise<SoundGifEntity> {
-    this.logger.log('SoundGifAdapter > getOne > start');
+    this.logger.log("SoundGifAdapter > getOne > start");
     return await this.soundGifRepository.findOne(whereOptions);
   }
 
   public async findMostShared(): Promise<SoundGifEntity[]> {
-    this.logger.log('SoundGifAdapter > findMostShared > start');
+    this.logger.log("SoundGifAdapter > findMostShared > start");
     return await this.soundGifRepository.find({
       order: {
-        sharedCount: 'DESC',
+        sharedCount: "DESC",
       },
     });
   }
 
+  public async getAllCategories(): Promise<string[]> {
+    this.logger.log("SoundGifAdapter > getAllCategories > start");
+    const allSoundGifs = await this.soundGifRepository.find({});
+    const allCategories = allSoundGifs.map(soundGif => soundGif.categories).flat();
+    const allCategoriesWithoutDuplicatedValues = Array.from(new Set(allCategories));
+    return allCategoriesWithoutDuplicatedValues;
+  }
+
   public async create(
-    payload: Partial<SoundGifEntity> & SoundGifEntityMandatoryFields,
+    payload: Partial<SoundGifEntity> & SoundGifEntityMandatoryFields
   ): Promise<SoundGifEntity> {
     this.logger.log(`SoundGifAdapter > create > called with ${payload}`);
     return await this.soundGifRepository.create(payload).save();
