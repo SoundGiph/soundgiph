b0fd8e97b28259917015b23c694622ea1254d2db
eMassou <erwan@libeo.io>
2022-03-27 20:41:37 +0200

    fix(api): remove  useless query

--- web/hooks/api/useApi.hook.ts
+++ web/hooks/api/useApi.hook.ts
@@ -1,70 +1,48 @@
 import { create } from "apisauce";
-import {
-  CREATE_SOUND_GIF,
-  FIND_MOST_RECENT_SOUND_GIF_QUERY,
-  FIND_MOST_SHARED_SOUND_GIF_QUERY,
-  FIND_SOUND_GIF_QUERY,
-  GET_ONE_SOUND_GIF
-} from "../../constants/constants";
+import { CREATE_SOUND_GIF, FIND_SOUND_GIF_QUERY } from "../../constants/constants";
 import { SoundgifDTO } from "../../domain/sound-gif.dto";
 
-export const useApi = (buildingTimeApiUrl: string, runningTimeApiUrl: string): {
-  findSoundGif: (fulltext: string) => Promise<SoundgifDTO[]>;
-  findMostRecentSoundGif: () => Promise<SoundgifDTO[]>;
-  findMostSharedSoundGif: () => Promise<SoundgifDTO[]>;
-  getOneSoundGif: (id: string) => Promise<SoundgifDTO|undefined>;
+interface SearchFilter {
+  category?: string;
+  reaction?: string;
+  mostShared?: boolean;
+  mostRecent?: boolean;
+}
+
+interface FindSoundGifsPayload {
+  filters?: SearchFilter;
+  fulltext?: string;
+}
+
+export const useApi = (
+  runningTimeApiUrl: string
+): {
+  findSoundGif: (payload: FindSoundGifsPayload) => Promise<SoundgifDTO[]>;
   createSoundGif: (payload: Omit<SoundgifDTO, "id">) => Promise<SoundgifDTO[]>;
 } => {
-
-
-  const building_time_api = create({
-    baseURL: buildingTimeApiUrl,
-  });
-
-  const running_time_api = create({
+  const api = create({
     baseURL: runningTimeApiUrl,
   });
 
   const createSoundGif = async (payload: Omit<SoundgifDTO, "id">): Promise<SoundgifDTO[]> => {
-    const { data } = await running_time_api.post<SoundgifDTO[]>(CREATE_SOUND_GIF, payload);
+    const { data } = await api.post<SoundgifDTO[]>(CREATE_SOUND_GIF, payload);
     return data ?? [];
   };
 
-  const findSoundGif = async (fulltext: string): Promise<SoundgifDTO[]> => {
-
-    if (fulltext != "") {
-      const { data } = await running_time_api.post<SoundgifDTO[]>(FIND_SOUND_GIF_QUERY, {
-        fulltext
-      });
-      return data ?? [];
-    }
-    return []
-  };
-
-  const findMostRecentSoundGif = async (): Promise<SoundgifDTO[]> => {
-
-    const { data } = await building_time_api.get<SoundgifDTO[]>(FIND_MOST_RECENT_SOUND_GIF_QUERY);
+  const findSoundGif = async (payload: FindSoundGifsPayload): Promise<SoundgifDTO[]> => {
+    const { fulltext, filters } = payload;
+    const { data } = await api.post<SoundgifDTO[]>(FIND_SOUND_GIF_QUERY, {
+      fulltext,
+      filters,
+    });
     return data ?? [];
   };
 
-  const findMostSharedSoundGif = async (): Promise<SoundgifDTO[]> => {
-    const { data } = await building_time_api.get<SoundgifDTO[]>(FIND_MOST_SHARED_SOUND_GIF_QUERY);
-    return data ?? [];
+  const getAllCategories = () => {
+    return ["mostShared", "mostRecent"];
   };
-
-  const getOneSoundGif = async (id: string): Promise<SoundgifDTO|undefined> => {
-    const { data, ok } = await building_time_api.get<SoundgifDTO>(`${GET_ONE_SOUND_GIF}/${id}`);
-    if (ok) {
-      return data ?? {} as SoundgifDTO
-    }
-    return 
-  };
-
   return {
     createSoundGif,
     findSoundGif,
-    findMostRecentSoundGif,
-    findMostSharedSoundGif,
-    getOneSoundGif,
   };
 };
