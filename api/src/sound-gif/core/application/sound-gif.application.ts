75e48e775dc278b07616ed5be4b60ecf04d43efe
eMassou <erwan@libeo.io>
2022-04-01 14:47:50 +0200

    feature(): added query

--- api/src/sound-gif/core/application/sound-gif.application.ts
+++ api/src/sound-gif/core/application/sound-gif.application.ts
@@ -3,6 +3,7 @@ import { FindMostRecentSoundGifQueryHandler } from "./queries/find-most-recent-s
 import { FindMostSharedSoundGifQueryHandler } from "./queries/find-most-shared-sound-gif/find-most-shared-sound-gif.query-handler";
 import { GetOneSoundGifQueryHandler } from "./queries/find-one-sound-gif/find-one-sound-gif.query-handler";
 import { FindSoundGifQueryHandler } from "./queries/find-sound-gif/find-sound-gif.query-handler";
+import { GetAllCategoriesWithSoundgifsQueryHandler } from "./queries/get-all-categories-with-soundgifs/get-all-categories-with-soundgifs.command-handler";
 import { GetAllCategoriesQueryHandler } from "./queries/get-all-categories/get-all-categories.query-handler";
 
 const SoundGifQueryHandler = [
@@ -11,6 +12,7 @@ const SoundGifQueryHandler = [
   FindMostRecentSoundGifQueryHandler,
   FindMostSharedSoundGifQueryHandler,
   GetAllCategoriesQueryHandler,
+  GetAllCategoriesWithSoundgifsQueryHandler,
 ];
 
 const SoundGifCommandHandler = [CreateSoundGifCommandHandler];
