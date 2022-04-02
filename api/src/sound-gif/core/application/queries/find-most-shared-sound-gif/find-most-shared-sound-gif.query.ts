bb2b67a91b47974da7a79bb0f28caebb02fbb9f5
eMassou <erwan@libeo.io>
2022-04-02 13:02:18 +0200

    refacto(): remove useless queries

--- api/src/sound-gif/core/application/queries/find-most-shared-sound-gif/find-most-shared-sound-gif.query.ts
+++
@@ -1,8 +0,0 @@
-import { IQuery, IQueryResult } from "@nestjs/cqrs";
-import { SoundGifEntity } from "../../../domain/sound-gif.entity";
-
-export class FindMostSharedSoundGifQuery implements IQuery {}
-
-export class FindMostSharedSoundGifQueryResult implements IQueryResult {
-  constructor(public readonly soundGifs: SoundGifEntity[]) {}
-}
--- api/src/sound-gif/interface/controllers/find-sound-gif.controller.ts
+++ api/src/sound-gif/interface/controllers/find-sound-gif.controller.ts
@@ -1,34 +1,21 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { QueryBus } from "@nestjs/cqrs";
 import "multer";
-import {
-  GetOneSoundGifQuery,
-  GetOneSoundGifQueryResult,
-} from "../../core/application/queries/find-one-sound-gif/find-one-sound-gif.query";
-import {
-  FindMostRecentSoundGifQuery,
-  FindMostRecentSoundGifQueryResult,
-} from "../../core/application/queries/find-most-recent-sound-gif/find-most-recent-sound-gif.query";
-import {
-  FindMostSharedSoundGifQuery,
-  FindMostSharedSoundGifQueryResult,
-} from "../../core/application/queries/find-most-shared-sound-gif/find-most-shared-sound-gif.query";
 import {
   FindSoundGifPayload,
   FindSoundGifQuery,
   FindSoundGifQueryResult,
 } from "../../core/application/queries/find-sound-gif/find-sound-gif.query";
-import { SoundGifEntity } from "../../core/domain/sound-gif.entity";
-import {
-  GetAllCategoriesQuery,
-  GetAllCategoriesQueryResult,
-} from "src/sound-gif/core/application/queries/get-all-categories/get-all-categories.query";
 import {
   CategoriesWithSoundgifs,
   GetAllCategoriesWithSoundgifsQuery,
   GetAllCategoriesWithSoundgifsQueryResult,
-  // eslint-disable-next-line max-len
-} from "src/sound-gif/core/application/queries/get-all-categories-with-soundgifs/get-all-categories-with-soundgifs.command";
+} from "../../core/application/queries/get-all-categories-with-soundgifs/get-all-categories-with-soundgifs.command";
+import {
+  GetAllCategoriesQuery,
+  GetAllCategoriesQueryResult,
+} from "../../core/application/queries/get-all-categories/get-all-categories.query";
+import { SoundGifEntity } from "../../core/domain/sound-gif.entity";
 @Controller()
 export class FindSoundGifController {
   constructor(private readonly queryBus: QueryBus) {}
@@ -43,30 +30,6 @@ export class FindSoundGifController {
       })
     );
     return soundGifs;
-  }
-
-  @Get("/findMostRecentSoundGif")
-  async findMostRecent(): Promise<SoundGifEntity[]> {
-    const { soundGifs } = await this.queryBus.execute<FindMostRecentSoundGifQuery, FindMostRecentSoundGifQueryResult>(
-      new FindMostRecentSoundGifQuery()
-    );
-    return soundGifs;
-  }
-
-  @Get("/findMostSharedSoundGif")
-  async findMostShared(): Promise<SoundGifEntity[]> {
-    const { soundGifs } = await this.queryBus.execute<FindMostSharedSoundGifQuery, FindMostSharedSoundGifQueryResult>(
-      new FindMostSharedSoundGifQuery()
-    );
-    return soundGifs;
-  }
-
-  @Get("/getOneSoundGif/:id")
-  async getOne(@Param("id") id: SoundGifEntity["id"]): Promise<SoundGifEntity> {
-    const { soundGif } = await this.queryBus.execute<GetOneSoundGifQuery, GetOneSoundGifQueryResult>(
-      new GetOneSoundGifQuery({ id })
-    );
-    return soundGif;
   }
 
   @Get("/getAllCategories")
