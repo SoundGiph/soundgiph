15808d295dba045d50ae45836c959049297205ad
eMassou <erwan@libeo.io>
2022-04-02 20:34:01 +0200

    refacto: renamed correctly

--- api/src/sound-gif/core/application/queries/get-all-categories-with-soundgifs/get-all-categories-with-soundgifs.command.ts
+++ api/src/sound-gif/core/application/queries/get-all-categories-with-soundgifs/get-all-categories-with-soundgifs.command.ts
@@ -1,10 +1,10 @@
 import { IQuery, IQueryResult } from "@nestjs/cqrs";
 import { SoundGifEntity } from "src/sound-gif/core/domain/sound-gif.entity";
 
-export type CategoriesWithSoundgifs = { name: string; soundgifs: SoundGifEntity[] };
+export type CategoriesWithSoundGifs = { name: string; soundGifs: SoundGifEntity[] };
 
 export class GetAllCategoriesWithSoundGifsQuery implements IQuery {}
 
 export class GetAllCategoriesWithSoundGifsQueryResult implements IQueryResult {
-  constructor(public readonly categoriesWithSoundgifs: CategoriesWithSoundgifs[]) {}
+  constructor(public readonly categoriesWithSoundgifs: CategoriesWithSoundGifs[]) {}
 }
