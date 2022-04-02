4503f017b5338ca167b53d72138af041329ae2c7
eMassou <erwan@libeo.io>
2022-04-02 13:04:16 +0200

    refacto(): renamed properly

--- api/src/sound-gif/core/application/queries/get-all-categories-with-soundgifs/get-all-categories-with-soundgifs.command.ts
+++ api/src/sound-gif/core/application/queries/get-all-categories-with-soundgifs/get-all-categories-with-soundgifs.command.ts
@@ -3,8 +3,8 @@ import { SoundGifEntity } from "src/sound-gif/core/domain/sound-gif.entity";
 
 export type CategoriesWithSoundgifs = { [key: string]: SoundGifEntity[] };
 
-export class GetAllCategoriesWithSoundgifsQuery implements IQuery {}
+export class GetAllCategoriesWithSoundGifsQuery implements IQuery {}
 
-export class GetAllCategoriesWithSoundgifsQueryResult implements IQueryResult {
+export class GetAllCategoriesWithSoundGifsQueryResult implements IQueryResult {
   constructor(public readonly categoriesWithSoundgifs: CategoriesWithSoundgifs[]) {}
 }
--- api/src/sound-gif/core/application/sound-gif.application.ts
+++ api/src/sound-gif/core/application/sound-gif.application.ts
@@ -1,12 +1,12 @@
 import { CreateSoundGifCommandHandler } from "./commands/create-sound-gif/create-sound-gif.command-handler";
 import { FindSoundGifQueryHandler } from "./queries/find-sound-gif/find-sound-gif.query-handler";
-import { GetAllCategoriesWithSoundgifsQueryHandler } from "./queries/get-all-categories-with-soundgifs/get-all-categories-with-soundgifs.command-handler";
+import { GetAllCategoriesWithSoundGifsQueryHandler } from "./queries/get-all-categories-with-soundgifs/get-all-categories-with-soundgifs.command-handler";
 import { GetAllCategoriesQueryHandler } from "./queries/get-all-categories/get-all-categories.query-handler";
 
 const SoundGifQueryHandler = [
   FindSoundGifQueryHandler,
   GetAllCategoriesQueryHandler,
-  GetAllCategoriesWithSoundgifsQueryHandler,
+  GetAllCategoriesWithSoundGifsQueryHandler,
 ];
 
 const SoundGifCommandHandler = [CreateSoundGifCommandHandler];
