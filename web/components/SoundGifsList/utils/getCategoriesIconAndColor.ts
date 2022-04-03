f28b9bdc817ce1bbf1475b758aab16a67aebaad7
eMassou <erwan@libeo.io>
2022-04-03 20:03:06 +0200

    fix(): fix dynamic icon

--- web/components/SoundGifsList/utils/getCategoriesIconAndColor.ts
+++ web/components/SoundGifsList/utils/getCategoriesIconAndColor.ts
@@ -1,55 +1,63 @@
 export enum Categories {
   "mostShared" = "mostShared",
   "mostRecent" = "mostRecent",
-  "Actualité & politique" = "Actualité & politique",
+  "Actualite&politique" = "Actualite&politique",
   "Anime" = "Anime",
-  "Cinéma" = "Cinéma",
-  "Dessins animé" = "Dessins animés",
+  "Cinema" = "Cinema",
+  "Dessinsanime" = "Dessinsanime",
   "Gaming" = "Gaming",
   "Humoristes" = "Humoristes",
-  "Médias & réseaux sociaux" = "Médias & réseaux sociaux",
+  "Medias&reseauxsociaux" = "Medias&reseauxsociaux",
   "Memes" = "Memes",
   "Musique" = "Musique",
   "Sport" = "Sport",
-  "Télévision" = "Télévision",
+  "Television" = "Television",
 }
 
 export enum CategoriesIconName {
   "mostShared" = "FireIcon",
   "mostRecent" = "ClockIcon",
-  "Actualité & politique" = "AcademicCap",
-  "Anime" = "Eye",
-  "Cinéma" = "Film",
-  "Dessins animés" = "Photograph",
-  "Gaming" = "Puzzle",
-  "Humoristes" = "EmojiHappy",
-  "Médias & réseaux sociaux" = "Globe",
-  "Memes" = "Chat",
-  "Musique" = "MusicNote",
-  "Sport" = "Speakerphone",
-  "Télévision" = "DesktopComputer",
+  "Actualite&politique" = "AcademicCapIcon",
+  "Anime" = "EyeIcon",
+  "Cinema" = "FilmIcon",
+  "Dessinsanime" = "PhotographIcon",
+  "Gaming" = "PuzzleIcon",
+  "Humoristes" = "EmojiHappyIcon",
+  "Medias&reseauxsociaux" = "GlobeIcon",
+  "Memes" = "ChatIcon",
+  "Musique" = "MusicNoteIcon",
+  "Sport" = "SpeakerphoneIcon",
+  "Television" = "DesktopComputerIcon",
 }
 
 export enum CategoriesIconColor {
   "mostShared" = "#6565F1",
   "mostRecent" = "#E449A3",
-  "Actualité & politique" = "white",
-  "Anime" = "green",
-  "Cinéma" = "red",
-  "Dessins animés" = "blue",
-  "Gaming" = "purple",
-  "Humoristes" = "yellow",
-  "Médias & réseaux sociaux" = "green",
-  "Memes" = "white",
-  "Musique" = "red",
-  "Sport" = "white",
-  "Télévision" = "brown",
+  "Actualite&politique" = "white",
+  "Anime" = "#E449A3",
+  "Cinema" = "#FF5733",
+  "Dessinsanime" = "#E449A3",
+  "Gaming" = "#571845",
+  "Humoristes" = "#FFC300",
+  "Medias&reseauxsociaux" = "#571845",
+  "Memes" = "#FF5733",
+  "Musique" = "#FF5733",
+  "Sport" = "#FFC300",
+  "Television" = "#FFC300",
 }
 
-export const getIconNameByCategory = (category: Categories): string => {
-  return CategoriesIconName[category];
+const formatCategory = (category: string): string => {
+  return category
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/\s/g, "");
+};
+export const getIconNameByCategory = (category: string): string => {
+  const formattedCategory = formatCategory(category);
+  return CategoriesIconName[formattedCategory as Categories];
 };
 
 export const getIconColorByCategory = (category: Categories): string => {
-  return CategoriesIconColor[category];
+  const formattedCategory = formatCategory(category);
+  return CategoriesIconColor[formattedCategory as Categories];
 };
