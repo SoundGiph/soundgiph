caf764b4ec5c4054cf1b882851ec8ec3d8c40cc0
Stephane Manganneau <manganneaustephane@gmail.com>
2022-04-04 01:35:32 +0200

    fix conflicts

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -5,7 +5,6 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
-import { useVozoApp } from "../context/useVozoApp.hook";
 import { VozoAppProvider } from "../context/VozoAppProvider";
 import "../styles/globals.css";
 
