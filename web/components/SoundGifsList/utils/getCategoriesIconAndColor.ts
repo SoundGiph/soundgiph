d855689781e9ed30f9ee7c9b70ed4b81a24b9c22
eMassou <erwan@libeo.io>
2022-04-03 18:26:11 +0200

    feature(): added util

--- web/components/SoundGifsList/utils/getCategoriesIconAndColor.ts
+++ web/components/SoundGifsList/utils/getCategoriesIconAndColor.ts
@@ -1,18 +1,22 @@
 export enum Categories {
-  "Actualité & politique",
-  "Anime",
-  "Cinéma",
-  "Dessins animés",
-  "Gaming",
-  "Humoristes",
-  "Médias & réseaux sociaux",
-  "Memes",
-  "Musique",
-  "Sport",
-  "Télévision",
+  "mostShared" = "mostShared",
+  "mostRecent" = "mostRecent",
+  "Actualité & politique" = "Actualité & politique",
+  "Anime" = "Anime",
+  "Cinéma" = "Cinéma",
+  "Dessins animé" = "Dessins animés",
+  "Gaming" = "Gaming",
+  "Humoristes" = "Humoristes",
+  "Médias & réseaux sociaux" = "Médias & réseaux sociaux",
+  "Memes" = "Memes",
+  "Musique" = "Musique",
+  "Sport" = "Sport",
+  "Télévision" = "Télévision",
 }
 
 export enum CategoriesIconName {
+  "mostShared" = "FireIcon",
+  "mostRecent" = "ClockIcon",
   "Actualité & politique" = "AcademicCap",
   "Anime" = "Eye",
   "Cinéma" = "Film",
@@ -22,6 +26,29 @@ export enum CategoriesIconName {
   "Médias & réseaux sociaux" = "Globe",
   "Memes" = "Chat",
   "Musique" = "MusicNote",
-  "Sport" = ,
-  "Télévision",
+  "Sport" = "Speakerphone",
+  "Télévision" = "DesktopComputer",
 }
+
+export enum CategoriesIconColor {
+  "mostShared" = "#6565F1",
+  "mostRecent" = "#E449A3",
+  "Actualité & politique" = "white",
+  "Anime" = "green",
+  "Cinéma" = "red",
+  "Dessins animés" = "blue",
+  "Gaming" = "purple",
+  "Humoristes" = "yellow",
+  "Médias & réseaux sociaux" = "green",
+  "Memes" = "white",
+  "Musique" = "red",
+  "Sport" = "white",
+  "Télévision" = "brown",
+}
+
+export const getIconNameAndColorByCategory = (category: Categories): { iconName: string; color: string } => {
+  return {
+    iconName: CategoriesIconName[category],
+    color: CategoriesIconColor[category],
+  };
+};
