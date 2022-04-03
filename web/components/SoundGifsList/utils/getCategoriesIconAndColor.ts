e7b6da43ced7507b9834c44398313a416da630df
eMassou <erwan@libeo.io>
2022-04-03 18:03:01 +0200

    wip

---
+++ web/components/SoundGifsList/utils/getCategoriesIconAndColor.ts
@@ -0,0 +1,27 @@
+export enum Categories {
+  "Actualité & politique",
+  "Anime",
+  "Cinéma",
+  "Dessins animés",
+  "Gaming",
+  "Humoristes",
+  "Médias & réseaux sociaux",
+  "Memes",
+  "Musique",
+  "Sport",
+  "Télévision",
+}
+
+export enum CategoriesIconName {
+  "Actualité & politique" = "AcademicCap",
+  "Anime" = "Eye",
+  "Cinéma" = "Film",
+  "Dessins animés" = "Photograph",
+  "Gaming",
+  "Humoristes",
+  "Médias & réseaux sociaux",
+  "Memes",
+  "Musique",
+  "Sport",
+  "Télévision",
+}
