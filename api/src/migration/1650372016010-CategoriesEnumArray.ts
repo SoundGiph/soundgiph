ddd67a792226800a3da7ac10eac444175e777dc0
eMassou <erwan@libeo.io>
2022-04-19 14:41:22 +0200

    feature: added migration

---
+++ api/src/migration/1650372016010-CategoriesEnumArray.ts
@@ -0,0 +1,18 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CategoriesEnumArray1650372016010 implements MigrationInterface {
+    name = 'CategoriesEnumArray1650372016010'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "sound_gif" DROP COLUMN "categories"`);
+        await queryRunner.query(`CREATE TYPE "public"."sound_gif_categories_enum" AS ENUM('News', 'Anime', 'Movies', 'Cartoons', 'Gaming', 'Comedy', 'Social', 'Memes', 'Music', 'Sport', 'TV')`);
+        await queryRunner.query(`ALTER TABLE "sound_gif" ADD "categories" "public"."sound_gif_categories_enum" NOT NULL DEFAULT ''`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "sound_gif" DROP COLUMN "categories"`);
+        await queryRunner.query(`DROP TYPE "public"."sound_gif_categories_enum"`);
+        await queryRunner.query(`ALTER TABLE "sound_gif" ADD "categories" text NOT NULL`);
+    }
+
+}
