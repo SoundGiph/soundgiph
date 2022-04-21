39478489b84af7b01bfdcb0991bb09da2f2ad361
Stephane Manganneau <manganneaustephane@gmail.com>
2022-04-22 00:28:41 +0200

    fix: set max size json to 3mb

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,8 +5,8 @@ import { urlencoded, json } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(json({ limit: '10mb' }));
-  app.use(urlencoded({ extended: true, limit: '10mb' }));
+  app.use(json({ limit: '3mb' }));
+  app.use(urlencoded({ extended: true, limit: '3mb' }));
   await app.listen(3000);
 }
 
