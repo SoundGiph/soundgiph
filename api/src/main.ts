7021a06112a1c700dc301ce6bfa4bfe6a17259dc
Stephane Manganneau <manganneaustephane@gmail.com>
2022-04-22 00:26:12 +0200

    fix: set max size json to 10mb

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ extended: true, limit: '10mb' }));
   await app.listen(3000);
 }
 
