86541009b9b423ec9c56267ed8f8421af13b2509
eMassou <erwan@libeo.io>
2022-04-22 16:42:35 +0200

    FIX TEST

--- api/test/sound-gif/get-all-categories/get-all-categories.e2e.spec.ts
+++ api/test/sound-gif/get-all-categories/get-all-categories.e2e.spec.ts
@@ -3,18 +3,18 @@ import { Test, TestingModule } from "@nestjs/testing";
 import * as request from "supertest";
 import { Connection } from "typeorm";
 import { AppModule } from "../../../src/app/app.module";
-import { SoundGifEntity } from "../../../src/sound-gif/core/domain/sound-gif.entity";
+import { Categories, SoundGifEntity } from "../../../src/sound-gif/core/domain/sound-gif.entity";
 import { soundGifFixtureFactory } from "../../../src/sound-gif/core/domain/sound-gif.fixture.factory";
 
 const soundGifFixtures = [
-  soundGifFixtureFactory({ description: "sch", categories: ["rap", "music"] }),
-  soundGifFixtureFactory({ tags: ["hamza", "rap"], categories: ["rap", "music"] }),
-  soundGifFixtureFactory({ title: "niska méchant", categories: ["rap", "music"] }),
-  soundGifFixtureFactory({ description: "sex", categories: ["sexy", "hot"] }),
-  soundGifFixtureFactory({ description: "bonjour", categories: ["cute", "music"] }),
+  soundGifFixtureFactory({ description: "sch", categories: [Categories.Music] }),
+  soundGifFixtureFactory({ tags: ["hamza", "rap"], categories: [Categories.Music] }),
+  soundGifFixtureFactory({ title: "niska méchant", categories: [Categories.Music] }),
+  soundGifFixtureFactory({ description: "sex", categories: [Categories.Movies] }),
+  soundGifFixtureFactory({ description: "bonjour", categories: [Categories.Music] }),
 ];
 
-const expectedCategories = ["cute", "hot", "music", "rap", "sexy"];
+const expectedCategories = [Categories.Movies, Categories.Music];
 describe("get all categories", () => {
   let app: NestApplication;
   let connection: Connection;
