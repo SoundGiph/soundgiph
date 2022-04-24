7aba6f94ba8334f0ba220d9c8b80ff73b6da9be2
Stephane Manganneau <manganneaustephane@gmail.com>
2022-04-24 12:39:56 +0200

    fix: remove unecessary checks

--- api/src/sound-gif/core/application/commands/create-sound-gif/create-sound-gif.command-handler.ts
+++ api/src/sound-gif/core/application/commands/create-sound-gif/create-sound-gif.command-handler.ts
@@ -1,6 +1,6 @@
-import { BadRequestException, Inject } from "@nestjs/common";
+import { Inject } from "@nestjs/common";
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
-import { Categories, SoundGifEntity } from "../../../domain/sound-gif.entity";
+import { SoundGifEntity } from "../../../domain/sound-gif.entity";
 import { SoundGifPort } from "../../ports/sound-gif.ports";
 import { CreateSoundGifCommand, CreateSoundGifCommandResult } from "./create-sound-gif.command";
 
@@ -9,21 +9,9 @@ export class CreateSoundGifCommandHandler implements ICommandHandler<CreateSound
   constructor(
     @Inject(SoundGifEntity)
     private readonly createSoundGifPort: Pick<SoundGifPort, "create">
-  ) {}
-
-  private checkCategoriesBeforeUpload = (categories: Categories[]): boolean => {
-    categories.forEach(category => {
-      if (!Object.values(Categories).includes(category)) return false;
-    });
-    return true;
-  };
+  ) { }
 
   public async execute({ payload }: CreateSoundGifCommand): Promise<CreateSoundGifCommandResult> {
-    if (!this.checkCategoriesBeforeUpload(payload.categories)) {
-      throw new BadRequestException(
-        `CreateSoundGifCommandHandler > fail >Invalid Category in payload.categories: ${payload.categories}`
-      );
-    }
     const createdSoundGif = await this.createSoundGifPort.create(payload);
     return new CreateSoundGifCommandResult(createdSoundGif);
   }
