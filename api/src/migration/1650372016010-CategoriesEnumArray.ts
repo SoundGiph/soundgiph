f6007574321ffb8b76ada74b4c972684ad585ee1
Stephane Manganneau <manganneaustephane@gmail.com>
2022-04-24 15:33:44 +0200

    fix: set default value for category to Comedy

--- api/src/migration/1650372016010-CategoriesEnumArray.ts
+++ api/src/migration/1650372016010-CategoriesEnumArray.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CategoriesEnumArray1650372016010 implements MigrationInterface {
     name = 'CategoriesEnumArray1650372016010'
@@ -6,7 +6,7 @@ export class CategoriesEnumArray1650372016010 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "sound_gif" DROP COLUMN "categories"`);
         await queryRunner.query(`CREATE TYPE "public"."sound_gif_categories_enum" AS ENUM('News', 'Anime', 'Movies', 'Cartoons', 'Gaming', 'Comedy', 'Social', 'Memes', 'Music', 'Sport', 'TV')`);
-        await queryRunner.query(`ALTER TABLE "sound_gif" ADD "categories" "public"."sound_gif_categories_enum" NOT NULL DEFAULT ''`);
+        await queryRunner.query(`ALTER TABLE "sound_gif" ADD "categories" "public"."sound_gif_categories_enum" NOT NULL DEFAULT 'Comedy'`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
