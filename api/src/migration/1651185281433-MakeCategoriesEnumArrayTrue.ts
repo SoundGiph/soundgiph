d2d7eebaffa035a0ffb9f8afc9d4a9e4f2f952fb
Stephane Manganneau <manganneaustephane@gmail.com>
2022-04-29 01:13:25 +0200

    feat: make categories an array of enum

---
+++ api/src/migration/1651185281433-MakeCategoriesEnumArrayTrue.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class MakeCategoriesEnumArrayTrue1651185281433 implements MigrationInterface {
+    name = 'MakeCategoriesEnumArrayTrue1651185281433'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "sound_gif" DROP COLUMN "categories"`);
+        await queryRunner.query(`DROP TYPE "public"."sound_gif_categories_enum"`);
+        await queryRunner.query(`CREATE TYPE "public"."sound_gif_categories_enum" AS ENUM('News', 'Anime', 'Movies', 'Cartoons', 'Gaming', 'Comedy', 'Social', 'Memes', 'Music', 'Sport', 'TV')`);
+        await queryRunner.query(`ALTER TABLE "sound_gif" ADD "categories" "public"."sound_gif_categories_enum" array NOT NULL DEFAULT '{Comedy}'`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "sound_gif" DROP COLUMN "categories"`);
+        await queryRunner.query(`CREATE TYPE "public"."sound_gif_categories_enum" AS ENUM('News', 'Anime', 'Movies', 'Cartoons', 'Gaming', 'Comedy', 'Social', 'Memes', 'Music', 'Sport', 'TV')`);
+        await queryRunner.query(`ALTER TABLE "sound_gif" ADD "categories" "public"."sound_gif_categories_enum" NOT NULL DEFAULT 'Comedy'`);
+    }
+
+}
